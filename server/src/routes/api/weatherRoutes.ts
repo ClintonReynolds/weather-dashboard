16704dd837bf151606351e94db2a9e9b070ef681
clinton reynolds <eastwood188209@gmail.com>
2025-02-17 18:51:51 -0500

    added logic to htmlroutes and weather routes

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,19 +1,42 @@
 import { Router } from 'express';
+import weatherService from '../../service/weatherService';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  
   // TODO: GET weather data from city name
-  // TODO: save city to search history
+  try {
+    const cityName = req.body.city;
+    const weatherData = await WeatherService.getWeatherForCity(cityName);
+    await HistoryService.addCity(cityName);
+    // TODO: save city to search history
+    return res.status(200).json(weatherData);
+
+    
+  }catch(error) {
+   return res.status(500).json({ error: 'Failed to fetch weather data'});
+  }
 });
 
 // TODO: GET search history
-router.get('/history', async (req, res) => {});
+router.get('/history', async (req, res) => {
+try {
+  const history = await HistoryService.getCities();
+  return res.status(200).json(history);
+}catch(error){
+  return res.status(500).json({ error: 'Failed to retrieve search history' });
+}
+});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req, res) => {});
+router.delete('/history/:id', async (req, res) => {
+  const id = req.params.id;
+  await HistoryService.removeCity(id);
+  res.status(202).send();
+});
 
 export default router;
